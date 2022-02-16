14ca79a226c3017e7cbd53aaa229038103ecf845
nicolasblotta <fede87blotta@gmail.com>
2022-02-16 18:16:52 -0300

    Update resize.sh

--- resize.sh
+++ resize.sh
@@ -13,19 +13,17 @@ RED_ALERT="\e[41m"
 PARTED=$(echo print free | parted > /tmp/initial_parted.log)
 ESPACO_LIVRE=$(df -h | grep "sda"| awk '{print $4}'> /tmp/initial_df.log && cat /tmp/initial_parted.log)
 LIVRE_PARTED=$(echo grep "Free Space" /tmp/initial_parted.log |grep "GB" | awk '{print $3}')
-PARTITION=$(df -h | egrep "sda"| awk '{print $1}' > /tmp/initial_partition.log && cat /tmp/initial_partition.log)
-NUMERO_DE_PART=$(cat /tmp/initial_parted.log | grep "ext"|awk '{print $1}')
+PARTITION=$(df -h | grep "sda"| awk '{print $1}' > /tmp/initial_partition.log && cat /tmp/initial_partition.log)
+NUMERO_DE_PART=$(grep "ext"/tmp/initial_parted.log|awk '{print $1}')
 
-echo -e "${YELLOW}amarelo${RESET}
-
-echo -e "${YELLOW}Você possui ${RESET}${GREEN}${ESPACO_LIVRE}${RESET}${YELLOW} livres na sua partição ${RED}${]}${RESET}.\n"
-
+echo ${PARTED}
+echo -e "${YELLOW}Você possui ${RESET}${GREEN}${ESPACO_LIVRE}${RESET}${YELLOW} livres na sua partição ${RED}${PARTITION}${RESET}.\n"
 echo -e "${YELLOW}Esse valor pode ser extendido até ${RESET}${GREEN}${LIVRE_PARTED}${RESET}\n\n"
 #echo -e "${YELLOW} Deseja extender completamente? Yes/No${RESET}.\n\n"
-  read -p "Deseja extender completamente? Yes/No" resposta
+  read -r "Deseja extender completamente? Yes/No" resposta
         if [[ "$resposta" == "yes" ]] ; then
           echo -e "${GREEN}[OK] ${RESET}${YELLOW} Realizando resize da partição.. ${RESET}${RED}${PARTITION}${RESET}"
-          parted ${PARTITION} resizepart ${NUMERO_DE_PART} ${LIVRE_PARTED}
+          parted "${PARTITION}" resizepart "${NUMERO_DE_PART}" "${LIVRE_PARTED}"
           echo "${BLUE}[SUCESSO] ${RESET}${YELLOW}Agora você possui ${RESET}${GREEN}${ESPACO_LIVRE} na sua particão ${RESET}${RED}${PARTITION}${RESET}"
           
         else
