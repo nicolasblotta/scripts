78ca6622803ba34dd896a211889ed6c7643876e6
nicolasblotta <fede87blotta@gmail.com>
2022-02-17 17:50:55 -0300

    Update resources.sh

--- resources.sh
+++ resources.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env python3
 #!/usr/bin/env bash
 IP=`hostname -I | cut -d ' ' -f 1`
-UPTIME=`uptime`
+UPTIME=`uptime|awk -F, '{print $1}'`
 VCPU=`nproc`
 ESPACO=`echo "Filesystem      Size  Used Avail Use% Mounted on" && df -h | grep -E  "/dev/sd"`
 MEM=`python3 -c "import os;print(int(round(os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES') / 1024.0**3)))"`
 
 
-echo -e "\n++++++++++++++++++\n+++++ UPTIME +++++\n++++++++++++++++++;
-${UPTIME}\n++++++++++++++++++
-\n+++++++++++++++++\n+++++ VCPU ++++++\n+++++++++++++++++\n
-Quantidade de vcpu: ${VCPU}
-\n+++++++++++++++++++\n+ ESPAÇO EM DISCO +\n+++++++++++++++++++ 
-${ESPACO}
-\n++++++++++++++++++\n+++++ MEMORIA ++++\n++++++++++++++++++\nQuantidade de memoria: ${MEM} Gb\n
-\nO IP da sua máquina é: ${IP}\n"
+echo -e "
+\n
+++++++++++++++++++\n
++++++ UPTIME +++++\n
+++++++++++++++++++\n
+${UPTIME}\n
+++++++++++++++++++\n
++++++++++++++++++\n
++++++ VCPU ++++++\n
++++++++++++++++++\n
+Quantidade de vcpu: ${VCPU}\n
++++++++++++++++++++\n
++ ESPAÇO EM DISCO +\n
++++++++++++++++++++\n 
+${ESPACO}\n
+++++++++++++++++++\n
++++++ MEMORIA ++++\n
+++++++++++++++++++\n
+Quantidade de memoria: ${MEM} Gb\n
+++++++++++++++++++\n
+O IP da sua máquina é: ${IP}\n"
