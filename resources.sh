a7d0e8b24398fcaf129c13d72926e89eb10e3356
nicolasblotta <fede87blotta@gmail.com>
2022-02-17 17:42:46 -0300

    Update resources.sh

--- resources.sh
+++ resources.sh
@@ -4,7 +4,7 @@ IP=`hostname -I | cut -d ' ' -f 1`
 UPTIME=`uptime`
 VCPU=`nproc`
 ESPACO=`echo "Filesystem      Size  Used Avail Use% Mounted on" && df -h | grep -E  "/dev/sd"`
-MEM=`python3 -c "import os;print(int(round(os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES') / 1024.0**3)))")`
+MEM=`python3 -c "import os;print(int(round(os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES') / 1024.0**3)))"`
 
 
 echo -e "\n++++++++++++++++++\n+++++ UPTIME +++++\n++++++++++++++++++;
